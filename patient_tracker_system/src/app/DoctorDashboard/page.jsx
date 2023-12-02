c722e20a5b491529288b2194c3826ceb2009160d
DarzyD <dangbrian96@gmail.com>
2023-12-01 20:17:11 -0500

    Added rendering for doctor dashboard via test data, will have to expand it further later

--- patient_tracker_system/src/app/DoctorDashboard/page.jsx
+++ patient_tracker_system/src/app/DoctorDashboard/page.jsx
@@ -1,13 +1,13 @@
 'use client';
 import {useSession} from 'next-auth/react';
 import {SearchPatient} from './SearchPatient';
+import {Schedule} from './Schedule';
+
 export default function Page() {
     const {data: session} = useSession();
     return <>
         <center>Welcome {session?.user?.first} {session?.user?.last} </center>
         <center> <SearchPatient/> </center>
-        <div className='container' style={{display:"flex"}}>
-           
-        </div>
+        <Schedule/> 
     </>
 }
