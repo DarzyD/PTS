c40b0977f5e4bddc8bb63cc16c2b114f43def5c0
jinc0930 <jinc0930@gmail.com>
2023-12-13 23:47:31 -0500

    attempting backend routing for patientDashboard appointments

--- patient_tracker_system_backend/routes/patientRoute.js
+++ patient_tracker_system_backend/routes/patientRoute.js
@@ -4,3 +4,22 @@ import db from '../databaseConn.js';
 
 const patientRouter = express.Router();
 
+patientRouter.get("/", async (req, res) => {
+    if (!req.query?.user) {
+        // If no specific user is provided, fetch all patients' usernames
+        const patients = await db.query("SELECT username FROM patients");
+        res.status(200).json(patients);
+      } else {
+        // If a specific user is provided, fetch that patient's username
+        const { user } = req.query;
+        const patient = await db.query("SELECT username FROM patients WHERE user = $1", [user]);
+        
+        if (patient.rows.length > 0) {
+          res.status(200).json(patient.rows[0]);
+        } else {
+          res.status(404).json({ message: 'Patient not found' });
+        }
+      }
+}); 
+
+export default patientRouter;
